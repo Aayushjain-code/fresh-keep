8204cbf7c4014fc7e04db2f54c242a93859507a3
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-03-30 22:35:10 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import NotesPage from "./Pages/NotesPage/NotesPage";
+import LabelPage from "./Pages/LabelPage/LabelPage";
+import ArchivePage from "./Pages/ArchivePage/ArchivePage";
+import Login from "./Pages/AuthPages/Login";
+import Signup from "./Pages/AuthPages/Signup";
+
+import Navbar from "./Components/Header/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Footer from "./Components/Footer/Footer";
+
 
 function App() {
   return (
     <div className="App">
-      notesApp
+      <Navbar />
+      <Sidebar />
+      <Footer />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/notes" element={<NotesPage />} />
+        <Route path="/labels" element={<LabelPage />} />
+        <Route path="/archives" element={<ArchivePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+      </Routes>
     </div>
   );
 }
