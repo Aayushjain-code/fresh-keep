c744db18d1a7778317c2fec1c33fcdbad8d2a83a
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-04-04 11:30:43 +0530

    bugs fixed

--- src/Pages/NotesPage/NotesPage.jsx
+++ src/Pages/NotesPage/NotesPage.jsx
@@ -1,132 +1,173 @@
-import { useEffect, useState } from 'react'
-import NewNote from './NewNote/NewNote'
-import NoteCreate from './NoteCreate/NoteCreate'
-import "./NotesPage.css"
-
+import { useEffect, useState } from "react";
+import NewNote from "./NewNote/NewNote";
+import NoteCreate from "./NoteCreate/NoteCreate";
+import "./NotesPage.css";
 
 import { useAuth } from "../../Context/authContext";
 import { useNotes } from "../../Context/noteContext";
-import { EditCard } from '../../Components/EditCard/EditCard';
+import { EditCard } from "../../Components/EditCard/EditCard";
 
 const NotesPage = () => {
+  const { authState } = useAuth();
+  const { notes } = authState;
+  const { getNotes, addNote } = useNotes();
 
-	const { authState } = useAuth();
-	const { notes } = authState;
-	const { getNotes, addNote } = useNotes();
+  const order = { Low: 1, Medium: 2, High: 3 };
 
+  const [labels, setLabels] = useState({
+    LowToHigh: false,
+    HighToLow: false,
+    oldest: false,
+    newest: false,
+  });
+  const [filteredNotes, setFilteredNotes] = useState([notes]);
 
-	const order = { Low: 1, Medium: 2, High: 3 };
+  useEffect(() => {
+    getNotes();
+  }, []);
 
-	const [labels, setLabels] = useState({
+  useEffect(() => {
+    (function () {
+      let newData = [...notes];
+      if (labels.LowToHigh) {
+        newData.sort((a, b) => order[a.priority] - order[b.priority]);
+      }
+      if (labels.HighToLow) {
+        newData.sort((a, b) => order[b.priority] - order[a.priority]);
+      }
+      if (labels.oldest) {
+        newData.sort((a, b) => a.createdDate - b.createdDate);
+      }
+      if (labels.newest) {
+        newData.sort((a, b) => b.createdDate - a.createdDate);
+      }
+      setFilteredNotes(newData);
+    })();
+  }, [notes, labels]);
 
-		LowToHigh: false,
-		HighToLow: false,
-		oldest: false,
-		newest: false,
-	});
-	const [filteredNotes, setFilteredNotes] = useState([notes]);
+  const [edit, setEdit] = useState({
+    isEdit: false,
+    editItem: {
+      _id: null,
+      pinned: false,
+      title: "",
+      description: "",
+      tag: "Tag",
+      priority: "Priority",
+      selectedBackgroundColor: "#faf8f8",
+    },
+  });
 
-	useEffect(() => {
-		getNotes();
-	}, []);
+  const [pinnedNotes, setPinnedNotes] = useState([]);
+  const [showNotes, setShowNotes] = useState(false);
 
-	useEffect(() => {
-		(function () {
-			let newData = [...notes];
-			if (labels.LowToHigh) {
-				newData.sort((a, b) => order[a.priority] - order[b.priority]);
-			}
-			if (labels.HighToLow) {
-				newData.sort((a, b) => order[b.priority] - order[a.priority]);
-			}
-			if (labels.oldest) {
-				newData.sort((a, b) => a.createdDate - b.createdDate)
-			}
-			if (labels.newest) {
-				newData.sort((a, b) => b.createdDate - a.createdDate)
+  useEffect(() => {
+    setPinnedNotes(notes.filter((item) => item.pinned));
+  }, [notes]);
 
-			}
-			setFilteredNotes(newData);
-		})();
-	}, [notes, labels]);
+  return (
+    <>
+      <div className="main-container">
+        <div className="filter-container">
+          <span className="filter-sub-heading">
+            <label style={{ fontSize: "1.8rem" }}>Priority Sorting: </label>
+            <br />
+            <label>
+              <input
+                type="radio"
+                name="group1"
+                onClick={() =>
+                  setLabels({ ...labels, HighToLow: true, LowToHigh: false })
+                }
+              />
+              <span>High to Low</span>
+            </label>
 
-	const [edit, setEdit] = useState({
-		isEdit: false,
-		editItem: {
-			_id: null,
-			pinned: false,
-			title: "",
-			description: "",
-			tag: "Tag",
-			priority: "Priority",
-			selectedBackgroundColor: "#faf8f8",
-		}
-	});
+            <br />
+            <label>
+              <input
+                type="radio"
+                name="group1"
+                onClick={() =>
+                  setLabels({ ...labels, HighToLow: false, LowToHigh: true })
+                }
+              />
+              <span>Low to High</span>
+            </label>
+          </span>
+          <span className="filter-sub-heading">
+            <label style={{ fontSize: "1.8rem" }}>Sort By Date: </label>
+            <br />
+            <label>
+              <input
+                type="radio"
+                name="group1"
+                onClick={() =>
+                  setLabels({ ...labels, oldest: true, newest: false })
+                }
+              />
+              <span>Oldest</span>
+            </label>
+            <br />
+            <label>
+              <input
+                type="radio"
+                name="group1"
+                onClick={() =>
+                  setLabels({ ...labels, oldest: false, newest: true })
+                }
+              />
+              <span>Newest</span>
+            </label>
+          </span>
+          <button
+            className="button primary"
+            onClick={() => setShowNotes(!showNotes)}
+          >
+            Show {showNotes ? `All ` : `Only Pinned `}Notes
+          </button>
+        </div>
 
-	return (
-		<>
-			<div className="main-container">
-				<div className="filter-container">
-					<span className="filter-sub-heading">
-						<label style={{ fontSize: '1.8rem' }}>Priority Sorting: </label>
-						<br />
-						<label>
-							<input type="radio" name="group1"
-								onClick={() =>
-									setLabels({ ...labels, HighToLow: true, LowToHigh: false })
-								}
-							/> <span>High to Low</span>
-						</label>
+        <div className="product-container">
+          <NoteCreate />
+          {console.log(notes)}
+        </div>
+        <section className="cards" id="cards" style={{ minWidth: "10rem" }}>
+          {showNotes ? (
+            <div
+              className="box-container flex-row-container2 "
+              style={{ minWidth: "130rem", marginLeft: "4rem" }}
+            >
+              {pinnedNotes.map((note) => (
+                <NewNote
+                  key={note._id}
+                  note={note}
+                  edit={edit}
+                  setEdit={setEdit}
+                />
+              ))}
+            </div>
+          ) : (
+            <div
+              className="box-container flex-row-container2 "
+              style={{ minWidth: "130rem", marginLeft: "4rem" }}
+            >
+              {notes.map((note) => (
+                <NewNote
+                  key={note._id}
+                  note={note}
+                  edit={edit}
+                  setEdit={setEdit}
+                />
+              ))}
+            </div>
+          )}
 
-						<br />
-						<label>
-							<input type="radio" name="group1"
-								onClick={() =>
-									setLabels({ ...labels, HighToLow: false, LowToHigh: true })
-								}
-							/> <span>Low to High</span>
-						</label>
-					</span>
-					<span className="filter-sub-heading">
-						<label style={{ fontSize: '1.8rem' }}>Sort By Date: </label>
-						<br />
-						<label>
-							<input type="radio" name="group1"
-								onClick={() =>
-									setLabels({ ...labels, oldest: true, newest: false })
-								}
-							/> <span>Oldest</span>
-						</label>
-						<br />
-						<label>
-							<input type="radio" name="group1"
-								onClick={() =>
-									setLabels({ ...labels, oldest: false, newest: true })
-								}
-							/> <span>Newest</span>
-						</label>
+          {edit.isEdit && <EditCard edit={edit} setEdit={setEdit} />}
+        </section>
+      </div>
+    </>
+  );
+};
 
-					</span>
-				</div>
-
-				<div className='product-container'>
-					<NoteCreate />
-
-				</div>
-				<section className="cards" id="cards" style={{ minWidth: "10rem" }}>
-					<div className="box-container flex-row-container2 " style={{ minWidth: "130rem", marginLeft: "4rem" }}>
-
-						{filteredNotes.map((note) => <NewNote key={note._id} note={note} edit={edit} setEdit={setEdit} />)}
-
-
-					</div>
-
-					{edit.isEdit && <EditCard edit={edit} setEdit={setEdit} />}
-				</section>
-			</div>
-
-		</>
-	)
-}
-
-export default NotesPage
+export default NotesPage;
